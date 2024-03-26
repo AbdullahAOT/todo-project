2fa88fd6a86d495a24a28268bc29305a06e38c58
AbdullahAOT <abdallahabuhalima.1.jo@gmail.com>
2024-03-26 05:48:32 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,17 @@
+/* When the home page is running you will do the following requirements in app.js:
+Ask the user to enter his/her gender as input. The answer should be either (male or female).
+Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero.
+Ask the user to confirm if he wants to skip the welcoming message.
+Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation.
+Continue working on the styling of the project. */
+
+
+// Ask the user to enter his/her name as input.
+let userName=prompt("Please enter your name:");
+
+// Ask the user to enter his/her gender as input. The answer should be either (male or female).
+let userGender=prompt("Please enter your gender (male or female):");
+
+
+
+//document.getElementsByClassName("userWelcomeMessage")[0].innerHTML="Welcome "+userName+" ("+userGender+")";
